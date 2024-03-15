bc2b077b13563802d37ca18287e4cb6969c94ad0
Pran4th <128239095+Pran4th@users.noreply.github.com>
2024-03-15 16:44:28 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="logo" src="https://i.pinimg.com/originals/24/9c/da/249cda9536381f86ce4c183feb9fda12.jpg" alt="Logo"></img>
+      <h1 className="header1">My Travel Journal</h1>
+    </div>
+    );
+}
