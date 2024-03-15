94311ae4104133a15dc5ca2b6acdfecdf143df0b
Pran4th <128239095+Pran4th@users.noreply.github.com>
2024-03-15 16:44:07 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,31 @@
+const Data = [
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Kodaki_fuji_frm_shojinko.jpg/1200px-Kodaki_fuji_frm_shojinko.jpg",
+    location: "JAPAN",
+    locationpoint: "View on Google maps",
+    title: "Mount Fuji",
+    date: "12 Jan,2021- 24 Jan,2021",
+    description: "Mount Fuji, or Fujisan as the Japanese call it, is located on Japan's largest island of Honshu. It has an elevation of 3,776m / 12,389ft and is Japan's tallest peak. It is a dormant volcano with the last eruption taking place in 1707.",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Sydney_Australia._%2821339175489%29.jpg",
+    location: "AUSTRALIA",
+    locationpoint: "View on Google maps",
+    title: "Sydney Opera House",
+    date: "27 Jan,2021- 8 Jun,2021",
+    description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney, New South Wales, Australia. Located on the foreshore of Sydney Harbour, it is widely regarded as one of the world's most famous and distinctive buildings and a masterpiece of 20th-century architecture.",
+  },
+  {
+    id: 3,
+    img: "https://assets.simpleviewcms.com/simpleview/image/upload/c_fill,g_xy_center,h_640,w_640,x_634,y_538/f_jpg/q_65/v1/clients/norway/large_View_over_Geirangerfjorden_from_Skagefla_Visit_A_lesund_Geiranger_Oddgeir_Visnes_0708cd8c-80d0-4a25-b19f-a726dfd01299.jpg?_a=BATCtdAA0",
+    location: "Norway",
+    locationpoint: "View on Google maps",
+    title: "Geirangerfjord",
+    date: "1 Oct,2021- 18 Nov,2021",
+    description: "The Geiranger Fjord (Norwegian: Geirangerfjorden) is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality. It is a 15-kilometre-long (91⁄2 mi) branch off the Sunnylvsfjorden, which is a branch off the Storfjorden (Great Fjord)."
+  }
+];
+
+export default Data;
